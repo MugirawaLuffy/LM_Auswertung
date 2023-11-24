a94fec846ab22f56d60142b88c15812a556fa455
Nils Drees <herrnielsson.nd@gmail.com>
2023-11-24 21:58:50 +0100

    Generate CDF and plot them accordingly

--- main.py
+++ main.py
@@ -17,21 +17,32 @@ def generate_from_path(path, save_to=None):
                               show_avg_under_plot=True,
                               show_variation_under_plot=True,
                               round_to=3,
-                              save_to=save_to+"both_under.png")
+                              save_to=save_to+calculation_wrapper.route_name+"_both_under.png")
     plot_deviation_per_sensor(calculation_wrapper,
                               show_avg_in_plot=True,
                               show_avg_under_plot=True,
                               show_variation_under_plot=True,
                               round_to=3,
-                              save_to=save_to + "avg_in_plot.png")
+                              save_to=save_to+calculation_wrapper.route_name + "avg_in_plot.png")
     plot_deviation_per_sensor(calculation_wrapper,
                               show_avg_in_plot=False,
                               show_avg_under_plot=False,
                               show_variation_under_plot=False,
                               round_to=3,
-                              save_to=save_to + "clean.png")
+                              save_to=save_to+calculation_wrapper.route_name + "clean.png")
 
-    print_expectation_per_sensor(calculation_wrapper)
+    save_to += "/cdf/" # <- following files should be saved in cdf subdir
+    for sensor in calculation_wrapper.sensors:
+        plot_cdf_and_confidence(calculation_wrapper.sensors[0], save_to=save_to + sensor.sensor_name + "_default.png")
+        plot_cdf_and_confidence(calculation_wrapper.sensors[0], save_to=save_to + sensor.sensor_name + "_clean.png",
+                                highlight_50_ci=False,
+                                highlight_95_ci=False,
+                                show_interval_in_graph=False)
+        plot_cdf_and_confidence(calculation_wrapper.sensors[0], save_to=save_to + sensor.sensor_name + "_subtle.png",
+                                highlight_50_ci=False,
+                                highlight_95_ci=False)
+
+
 
 
 def main():
@@ -47,8 +58,8 @@ def main():
     for file in files:
         counter += 1
         if not os.path.isdir(file):
-            os.makedirs(path + "/" + file.split(".")[0] + "_" + str(counter))
-            generate_from_path(file, path + "/" + file.split(".")[0] + "_" + str(counter) + "/" + file.split(".")[0])
+            os.makedirs(path + "/" + file.split(".")[0] + "_" + str(counter) + "/cdf")
+            generate_from_path(file, path + "/" + file.split(".")[0] + "_" + str(counter) + "/")
 
 
 if __name__ == '__main__':
--- plots.py
+++ plots.py
@@ -1,4 +1,6 @@
-from csv_repository import CalculationWrapper
+import numpy as np
+from scipy import stats
+from csv_repository import CalculationWrapper, SensorData
 import matplotlib.pyplot as plt
 
 
@@ -59,3 +61,67 @@ def plot_deviation_per_sensor(wrapper: CalculationWrapper,
         plt.savefig(save_to)
     else:
         plt.show()
+
+
+
+
+def plot_cdf_and_confidence(data: SensorData,
+                            show_interval_in_graph=True,
+                            show_interval_underneath=True,
+                            highlight_50_ci=True,
+                            highlight_95_ci=True,
+                            round_to=4,
+                            save_to=None):
+    # Extract relevant data
+    deviations = data.additional_payload.get("deviations", [])
+    expected_div = data.additional_payload.get("expected_div", 0)
+    deviance = data.additional_payload.get("deviance", 0)
+
+    # Plot the cumulative distribution function (CDF) of positioning error
+    x = np.sort(deviations)
+    y = np.arange(1, len(x) + 1) / len(x)
+
+    # Plot the CDF
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, marker='o', linestyle='-', color='b')
+    plt.title(f'CDF von {data.sensor_name}')
+    plt.xlabel('Positionierungsfehler')
+    plt.ylabel('Kumulative Wkt.')
+    plt.grid(True)
+
+    # Calculate positioning error with 50% and 95% confidence intervals based on the deviation from the expected_div
+    conf_interval_50 = list(stats.norm.interval(0.50, loc=expected_div, scale=deviance))
+    conf_interval_95 = list(stats.norm.interval(0.95, loc=expected_div, scale=deviance))
+
+    conf_interval_50[0] = round(conf_interval_50[0], round_to)
+    conf_interval_50[1] = round(conf_interval_50[1], round_to)
+    conf_interval_95[0] = round(conf_interval_95[0], round_to)
+    conf_interval_95[1] = round(conf_interval_95[1], round_to)
+
+    # print(f"Positioning Error with 50% confidence interval: {conf_interval_50}")
+    # print(f"Positioning Error with 95% confidence interval: {conf_interval_95}")
+
+    if show_interval_underneath:
+        plt.subplots_adjust(bottom=0.2)
+        confidences = (f"50% Konfidenzinterval: {conf_interval_50}\n"
+                       f"95% Konfidenzinterval: {conf_interval_95}")
+        plt.figtext(0.05, 0.01, confidences, fontsize=11, va="bottom", ha="left")
+
+    if show_interval_in_graph:
+        plt.axvline(x=conf_interval_50[0], color='y', linestyle='--', label='50% CI Lower')
+        plt.axvline(x=conf_interval_50[1], color='y', linestyle='--', label='50% CI Upper')
+        plt.axvline(x=conf_interval_95[0], color='r', linestyle='--', label='95% CI Lower')
+        plt.axvline(x=conf_interval_95[1], color='r', linestyle='--', label='95% CI Upper')
+
+        if highlight_95_ci:
+            plt.axvspan(conf_interval_95[0], conf_interval_95[1], color='r', alpha=0.3)
+
+        if highlight_50_ci:
+            plt.axvspan(conf_interval_50[0], conf_interval_50[1], color='y', alpha=0.3)
+
+    if save_to is not None:
+        plt.savefig(save_to)
+    else:
+        plt.show()
+
+    plt.close()
